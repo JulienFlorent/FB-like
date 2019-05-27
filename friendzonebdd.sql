0afa957d3830d205c319ed25af315ca3691e4035
Quentin <Colmenero>
2019-05-27 11:44:11 +0200

    added db
    latest ver

--- friendzonebdd.sql
+++ friendzonebdd.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 27 mai 2019 à 08:45
--- Version du serveur :  5.7.24
--- Version de PHP :  7.2.14
+-- Hôte : 127.0.0.1
+-- Généré le :  lun. 27 mai 2019 à 11:42
+-- Version du serveur :  10.1.38-MariaDB
+-- Version de PHP :  7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `bdd-facebook-esi`
+-- Base de données :  `friendzonebdd`
 --
 
 -- --------------------------------------------------------
@@ -28,12 +28,10 @@ SET time_zone = "+00:00";
 -- Structure de la table `actualités`
 --
 
-DROP TABLE IF EXISTS `actualités`;
-CREATE TABLE IF NOT EXISTS `actualités` (
+CREATE TABLE `actualités` (
   `IdActualité` int(15) NOT NULL,
   `TexteActu` varchar(255) NOT NULL,
-  `LienActualité` int(60) NOT NULL,
-  PRIMARY KEY (`IdActualité`)
+  `LienActualité` int(60) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -42,13 +40,11 @@ CREATE TABLE IF NOT EXISTS `actualités` (
 -- Structure de la table `amis`
 --
 
-DROP TABLE IF EXISTS `amis`;
-CREATE TABLE IF NOT EXISTS `amis` (
+CREATE TABLE `amis` (
   `IdAmi` int(15) NOT NULL,
   `NomAmi` varchar(40) NOT NULL,
   `PrénomAmi` varchar(40) NOT NULL,
-  `TypeAmi` int(15) NOT NULL,
-  PRIMARY KEY (`IdAmi`)
+  `TypeAmi` int(15) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -57,11 +53,9 @@ CREATE TABLE IF NOT EXISTS `amis` (
 -- Structure de la table `catégorieami`
 --
 
-DROP TABLE IF EXISTS `catégorieami`;
-CREATE TABLE IF NOT EXISTS `catégorieami` (
+CREATE TABLE `catégorieami` (
   `TypeAmi` int(15) NOT NULL,
-  `DésignationAmi` varchar(50) NOT NULL,
-  PRIMARY KEY (`TypeAmi`)
+  `DésignationAmi` varchar(50) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -70,12 +64,10 @@ CREATE TABLE IF NOT EXISTS `catégorieami` (
 -- Structure de la table `conversation`
 --
 
-DROP TABLE IF EXISTS `conversation`;
-CREATE TABLE IF NOT EXISTS `conversation` (
+CREATE TABLE `conversation` (
   `IdConversation` int(15) NOT NULL,
   `TexteConv` varchar(255) NOT NULL,
-  `IdUtilisateur` int(15) NOT NULL,
-  PRIMARY KEY (`IdConversation`)
+  `IdUtilisateur` int(15) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -84,16 +76,56 @@ CREATE TABLE IF NOT EXISTS `conversation` (
 -- Structure de la table `utilisateur`
 --
 
-DROP TABLE IF EXISTS `utilisateur`;
-CREATE TABLE IF NOT EXISTS `utilisateur` (
+CREATE TABLE `utilisateur` (
   `IdUtilisateur` int(15) NOT NULL,
   `NomUtilisateur` varchar(30) NOT NULL,
   `PrénomUtilisateur` varchar(30) NOT NULL,
-  `VilleUtilisateur` int(11) NOT NULL,
+  `AdresseMailUtilisateur` varchar(50) NOT NULL,
+  `VilleUtilisateur` varchar(50) NOT NULL,
   `IdAmi` int(15) NOT NULL,
-  `IdConversation` int(15) NOT NULL,
-  PRIMARY KEY (`IdUtilisateur`)
+  `IdConversation` int(15) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`IdUtilisateur`, `NomUtilisateur`, `PrénomUtilisateur`, `AdresseMailUtilisateur`, `VilleUtilisateur`, `IdAmi`, `IdConversation`) VALUES
+(0, 'La Poice', 'Morice', 'moricelapolice@gmail.com', 'Mauroce', 0, 0);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `actualités`
+--
+ALTER TABLE `actualités`
+  ADD PRIMARY KEY (`IdActualité`);
+
+--
+-- Index pour la table `amis`
+--
+ALTER TABLE `amis`
+  ADD PRIMARY KEY (`IdAmi`);
+
+--
+-- Index pour la table `catégorieami`
+--
+ALTER TABLE `catégorieami`
+  ADD PRIMARY KEY (`TypeAmi`);
+
+--
+-- Index pour la table `conversation`
+--
+ALTER TABLE `conversation`
+  ADD PRIMARY KEY (`IdConversation`);
+
+--
+-- Index pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD PRIMARY KEY (`IdUtilisateur`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
