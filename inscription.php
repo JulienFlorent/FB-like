4f3dc08150be35847f6859d602bcb48a9c879b23
Quentin <Colmenero>
2019-06-03 11:10:24 +0200

    maj php ajout php

---
+++ inscription.php
@@ -0,0 +1,30 @@
+<?php
+echo ' 
+<br>
+Pour vous inscrire, utilisez ce formunaire : 
+    <form action="" method="post">
+    <div>
+        <label for="NomUtilisateur">Nom :</label>
+        <input type="text" id="name" name="user_name">
+    </div>
+    <div>
+        <label for="PrenomUtilsateur">Prénom</label>
+        <input type="text" id="name" name="user_name">
+    </div>
+    <div>
+        <label for="VilleUtilisateur">Votre ville : </label>
+        <input type="text" id="VilleUtilisateur" name="VilleUtilisateur"></input>
+    </div>
+    <div>
+        <label for="MotDePasseUtilisateur">Mot de passe : </label>
+        <input type="text" id="MotDePasseUtilisateur" name="MotDePasseUtilisateur"></input>
+    </div>
+    <div>
+    <label for="MotDePasseUtilisateur"><label>
+    <input type="button" value="Inscription">
+</div>
+</form>       
+
+
+';
+?>
